140fce35fe3d1470a5d81eac38683416b3dee51b
sonlv94 <soncuc182304@gmail.com>
2024-07-29 16:48:44 +0900

    &&

--- app.py
+++ app.py
@@ -66,11 +66,11 @@ def search():
     ).all()
     return render_template('index.html', todos=tasks)
 
-if __name__ == "__main__":
-    # Waitress serverを使用してアプリケーションを起動
-    serve(app, host='0.0.0.0', port=8080)
+# if __name__ == "__main__":
+#     # Waitress serverを使用してアプリケーションを起動
+#     serve(app, host='0.0.0.0', port=8080)
 
 # 開発環境
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False)
     

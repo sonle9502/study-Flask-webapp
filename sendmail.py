316e413b7dafd031f29b1ba292e8e2b97fe2b65f
sonlv94 <soncuc182304@gmail.com>
2024-07-31 14:28:09 +0900

    add sendmail feature

--- sendmail.py
+++ sendmail.py
@@ -5,23 +5,23 @@ from models import db, Todo  # Import from models
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from flask import current_app
 
 def check_due_tasks():
-    now = datetime.now()
-    tasks = Todo.query.all()
-    for task in tasks:
-            subject = "Task Due Reminder"
-            body = f"Task '{task.content}' is due now!"
-            to = "soncuc182304@gmail.com"  # Replace with the recipient's email
-            send_email(subject, body, to)   
-
-# Schedule the task checker to run every minute
-schedule.every(1).minutes.do(check_due_tasks)
+    with current_app.app_context():
+        now = datetime.now()
+        tasks = Todo.query.all()
+        for task in tasks:
+            if task.due_date and task.due_date <= now:
+                subject = "Task Due Reminder"
+                body = f"Task '{task.content}{task.description}'  is due now!"
+                to = "soncuc182304@gmail.com"  # Replace with the recipient's email
+                send_email(subject, body, to)
 
 def send_email(subject, body, to):
     from_email = "soncuc182304@gmail.com"
-    password = "Future1234@#"
-    smtp_server = "smtp.example.com"
+    password = "vrtt wsgn ktup mmga "
+    smtp_server = "smtp.gmail.com"
     smtp_port = 587
 
     msg = MIMEMultipart()
